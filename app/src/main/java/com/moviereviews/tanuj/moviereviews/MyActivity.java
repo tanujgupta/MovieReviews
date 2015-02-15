c6b6eff8116acfc38558c71f7a8147ad82748922
Tanuj Gupta <tanuj@spoton.com>
2015-02-15 15:33:04 -0600

    added custom toolbar

--- app/src/main/java/com/moviereviews/tanuj/moviereviews/MyActivity.java
+++ app/src/main/java/com/moviereviews/tanuj/moviereviews/MyActivity.java
@@ -1,18 +1,23 @@
 package com.moviereviews.tanuj.moviereviews;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.support.v7.widget.Toolbar;
 
 
 public class MyActivity extends ActionBarActivity {
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
+
+        toolbar = (Toolbar) findViewById(R.id.app_bar);
+        setSupportActionBar(toolbar);
     }
 
 
