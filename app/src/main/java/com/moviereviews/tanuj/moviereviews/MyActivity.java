b877d3d5f5610e80621e5b841e9086d331801d10
Tanuj Gupta <tanuj@spoton.com>
2015-02-16 19:22:48 -0600

    added volley library in build file removed the custom navigation to bring icons into the swipe tabs

--- app/src/main/java/com/moviereviews/tanuj/moviereviews/MyActivity.java
+++ app/src/main/java/com/moviereviews/tanuj/moviereviews/MyActivity.java
@@ -49,13 +49,6 @@ public class MyActivity extends ActionBarActivity {
         mTabs = (SlidingTabLayout) findViewById(R.id.tabs);
         mPager.setAdapter(new MyPagerAdapter(getSupportFragmentManager()));
         mTabs.setDistributeEvenly(true);
-        mTabs.setCustomTabColorizer(new SlidingTabLayout.TabColorizer() {
-            @Override
-            public int getIndicatorColor(int position) {
-                return getResources().getColor(R.color.colorAccent);
-            }
-        });
-        mTabs.setCustomTabView(R.layout.custom_tab_view, R.id.tabText);
         mTabs.setViewPager(mPager);
 
 
@@ -65,7 +58,6 @@ public class MyActivity extends ActionBarActivity {
     class MyPagerAdapter extends FragmentPagerAdapter {
 
         String[] tabs;
-        int icons[] = {R.drawable.ic_action_home, R.drawable.ic_action_articles, R.drawable.ic_action_personal};
 
         public MyPagerAdapter(FragmentManager fm) {
 
@@ -76,12 +68,7 @@ public class MyActivity extends ActionBarActivity {
         @Override
         public CharSequence getPageTitle(int position) {
 
-            Drawable drawable =  getResources().getDrawable(icons[position]);
-            drawable.setBounds(0,0,36,36);
-            ImageSpan imageSpan =  new ImageSpan(drawable);
-            SpannableString spannableString =  new SpannableString(" ");
-            spannableString.setSpan(imageSpan, 0, spannableString.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-            return spannableString;
+            return tabs[position];
         }
 
         @Override
