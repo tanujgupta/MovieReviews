899e8bb16eae8c9ce20e6341c446a8ca4790cc6d
Tanuj Gupta <tanujgup@gmail.com>
2015-02-17 20:51:50 -0600

    added  some more static urls

--- app/src/main/java/com/moviereviews/tanuj/moviereviews/extras/UrlEndpoints.java
+++ app/src/main/java/com/moviereviews/tanuj/moviereviews/extras/UrlEndpoints.java
@@ -4,6 +4,8 @@ package com.moviereviews.tanuj.moviereviews.extras;
 public class UrlEndpoints {
 
     public static final String URL_BOX_OFFICE = "http://api.rottentomatoes.com/api/public/v1.0/lists/movies/box_office.json";
+    public static final String URL_UPCOMING = "http://api.rottentomatoes.com/api/public/v1.0/lists/movies/upcoming.json";
+    public static final String URL_IN_THEATERS = "http://api.rottentomatoes.com/api/public/v1.0/lists/movies/in_theaters.json";
     public static final String URL_CHAR_QUESTION = "?";
     public static final String URL_CHAR_AMEPERSAND = "&";
     public static final String URL_PARAM_API_KEY = "apikey=";
