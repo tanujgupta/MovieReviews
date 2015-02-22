5f4348a0b31582fad2b782916176f159dc9d23f3
Tanuj Gupta <tanujgup@gmail.com>
2015-02-22 15:41:39 -0600

    Added home button on detalied info activity

--- app/src/main/java/com/moviereviews/tanuj/moviereviews/activities/DetailedMovieInfo.java
+++ app/src/main/java/com/moviereviews/tanuj/moviereviews/activities/DetailedMovieInfo.java
@@ -2,8 +2,10 @@ package com.moviereviews.tanuj.moviereviews.activities;
 
 
 import android.os.Bundle;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import com.moviereviews.tanuj.moviereviews.R;
 
@@ -18,5 +20,18 @@ public class DetailedMovieInfo extends ActionBarActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.app_bar);
         setSupportActionBar(toolbar);
 
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (item.getItemId() == R.id.home)
+        {
+            NavUtils.navigateUpFromSameTask(this);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
